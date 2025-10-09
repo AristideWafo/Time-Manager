743b6b71b30bd8b69935cef8b14c5ebaab4ef91f
Valentin Cheel <valentin.cheel.pro@gmail.com>
2025-10-09 11:08:19 +0200

    initial front

---
+++ front/manger_time_front/src/app/view/pages/profile/profile.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-profile',
+    standalone: true,
+    template: `
+  <section class="profile">
+    <h1>Profile</h1>
+  </section>
+  `
+})
+export class ProfileComponent { }
+
+
