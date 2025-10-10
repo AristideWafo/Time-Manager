fdea6ddb73f61b5b424a495ec97a744b1f30744b
valentinCHEEL <156203235+valentinCHEEL@users.noreply.github.com>
2025-10-10 16:11:06 +0200

    Feat/front init (#24)
    * initial front
    
    * init front-end structure
    
    * refactor: réorganisation des dossiers et fichiers

---
+++ front/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+  <main>
+    <header class="brand-name" style="display: flex; align-items: center; gap: 10px; margin-bottom: 20px;">
+      <img class="brand-logo" src="assets/logo.svg" alt="logo" aria-hidden="true" style="height: 100px;">
+      <strong style="font-size: 1.5rem;">Manager Time</strong>
+    </header>
+
+    <section class="content" style="align-items: center; text-align: center;">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+  `,
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet]
+})
+export class AppComponent {
+  title = 'homes';
+}
