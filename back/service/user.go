a31f8e3b27bf49feb9c731c411312759cf50b92f
Heriatac1 <108985046+Heriatac1@users.noreply.github.com>
2025-10-16 15:18:57 +0200

    feat : Adding password hash to create user and login functions (#56)
    * feat : Adding password hash to create user and login functions
    
    * feat : Adding password hash to create user and login functions
    
    * feat : Adding password hash to create user and login functions
    
    * feat : Adding password hash to create user and login functions
    
    * feat : Adding password hash to create user and login functions
    
    ---------
    
    Co-authored-by: Perigo_v <erigo_v@etna-alternance.net>

--- back/service/user.go
+++ back/service/user.go
@@ -3,6 +3,8 @@ package service
 import (
 	"TimeManager/model"
 	"TimeManager/repository"
+	"errors"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
@@ -10,8 +12,23 @@ import (
 func Login(email string, password string) (*model.User, error) {
 
 	user := &model.User{}
-	filter := bson.D{{Key: "Email", Value: email}, {Key: "Password", Value: password}}
-	return user, repository.GetOneUser(user, filter)
+	filter := bson.D{{Key: "Email", Value: email}}
+
+	err := repository.GetOneUser(user, filter)
+	if err != nil {
+		return nil, fmt.Errorf("user not found: %w", err)
+	}
+	check, err := CheckPasswordHash([]byte(password), user.Password)
+
+	if !check {
+		return &model.User{}, errors.New("incorrect password")
+	}
+
+	if err != nil {
+		return &model.User{}, err
+	}
+
+	return user, err
 }
 
 func GetUserByID(_id bson.ObjectID) (*model.User, error) {
@@ -32,6 +49,18 @@ func CreateUser(first_name string, last_name string, email string, password stri
 		Role:      role,
 	}
 
+	password, err := HashPassword(password)
+
+	if err != nil {
+		return &model.User{}, err
+	}
+
+	if password == "" {
+		return &model.User{}, errors.New("hashing failed, password returned empty")
+	}
+
+	user.Password = password
+
 	if team_name == "" {
 		return user, repository.SaveUser(user)
 	}
@@ -39,7 +68,7 @@ func CreateUser(first_name string, last_name string, email string, password stri
 	team := &model.Team{}
 	filter := bson.D{{Key: "Name", Value: team_name}}
 
-	err := repository.GetOneTeam(team, filter)
+	err = repository.GetOneTeam(team, filter)
 
 	if err != nil {
 		return &model.User{}, err
@@ -55,6 +84,19 @@ func UpdateUserByID(_id bson.ObjectID, update bson.D) (*model.User, error) {
 	user := &model.User{}
 	filter := bson.D{{Key: "_id", Value: _id}}
 	err := repository.GetOneUser(user, filter)
+
+	for i := range update {
+		if update[i].Key == "Password" {
+			if password, ok := update[i].Value.(string); ok {
+				update[i].Value, err = HashPassword(password)
+				if err != nil {
+					return user, err
+				}
+			} else if !ok {
+				return user, errors.New("error : password is not a string")
+			}
+		}
+	}
 
 	if err != nil {
 		return user, err
