166f769c697f656a2e5d0b4b3069c43c172b9db1
Perigo_v <erigo_v@etna-alternance.net>
2025-10-27 10:43:05 +0100

    Adding test variables

--- back/service/hash_test.go
+++ back/service/hash_test.go
@@ -6,22 +6,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	testPassword    = "ThisIsAPassword"
+	badTestPassword = "ThisIsAWrongPassword"
+)
+
 func TestHashPassword(t *testing.T) {
-	hashed, err := HashPassword("ThisIsAPassword")
+	hashed, err := HashPassword(testPassword)
 	assert.NoError(t, err)
 	assert.Len(t, hashed, 60)
 }
 
 func TestCheckPasswordHash(t *testing.T) {
-	password := "ThisIsAPassword"
-	badPassword := "ThisIsAWrongPassword"
 	hash := "$2a$14$aJ7lrRHNeEEjhHkVx6Ldv.p3UpwpADFX8614EJp2cfSV/CPipBf02"
 
-	trueCheck, err := CheckPasswordHash([]byte(password), hash)
+	trueCheck, err := CheckPasswordHash([]byte(testPassword), hash)
 	assert.NoError(t, err)
 	assert.True(t, trueCheck)
 
-	falseCheck, err := CheckPasswordHash([]byte(badPassword), hash)
+	falseCheck, err := CheckPasswordHash([]byte(badTestPassword), hash)
 	assert.NoError(t, err)
 	assert.False(t, falseCheck)
 
